3fa61ffc4e73f21b2b5ca2602d436fd190c5ccb2
Ying <joseph.wy@outlook.com>
2020-06-29 17:11:32 -0700

    init workflow (#2)
    init the workflow with github action.
    
    * build binaries
    * build rpms
    * upload rpms to s3
    
    
    Co-authored-by: Ying Wang <wangmyin@amazon.com>

--- tools/packaging/linux/build.spec
+++ tools/packaging/linux/build.spec
@@ -1,10 +1,10 @@
 Summary:    AWS Opentelemetry Collector
-Name:       aoc
+Name:       %{RPM_NAME}
 Version:    %{VERSION}
 Release:    1
 License:    Amazon Software License. Copyright 2017 Amazon.com, Inc. or its affiliates. All Rights Reserved.
 Group:      Applications/AWS
-Source0:    aoc-%{VERSION}.tar.gz
+Source0:    %{RPM_NAME}-%{VERSION}.tar.gz
 
 %description
 This package provides daemon of AWS Opentelemetry Collector
@@ -13,7 +13,9 @@ This package provides daemon of AWS Opentelemetry Collector
 %setup -q
 
 %install
-cp -rfa * %{buildroot}
+rm -rf ${RPM_BUILD_ROOT}
+mkdir -p ${RPM_BUILD_ROOT}
+cp -rfa * ${RPM_BUILD_ROOT}
 
 %files
 %dir /opt/aws
@@ -33,3 +35,6 @@ cp -rfa * %{buildroot}
 #/etc/amazon/aws-opentelemetry-collector
 #/var/log/amazon/aws-opentelemetry-collector
 #/var/run/amazon/aws-opentelemetry-collector
+
+%clean
+rm -rf ${RPM_BUILD_ROOT}
--- tools/packaging/linux/create_rpm.sh
+++ tools/packaging/linux/create_rpm.sh
@@ -9,31 +9,35 @@ SPEC_FILE="tools/packaging/linux/build.spec"
 BUILD_ROOT="`pwd`/build/rpmbuild"
 WORK_DIR="`pwd`/build/rpmtar"
 VERSION=`cat VERSION`
+RPM_NAME=aws-opentelemetry-collector
+AOC_ROOT=${WORK_DIR}/${RPM_NAME}-${VERSION}
 
 echo "Creating rpmbuild workspace"
 mkdir -p ${BUILD_ROOT}/{RPMS,SRPMS,BUILD,SOURCES,SPECS}
-mkdir -p ${WORK_DIR}/aoc-${VERSION}/opt/aws/aws-opentelemetry-collector/logs
-mkdir -p ${WORK_DIR}/aoc-${VERSION}/opt/aws/aws-opentelemetry-collector/bin
-mkdir -p ${WORK_DIR}/aoc-${VERSION}/opt/aws/aws-opentelemetry-collector/etc
-mkdir -p ${WORK_DIR}/aoc-${VERSION}/opt/aws/aws-opentelemetry-collector/var
-mkdir -p ${WORK_DIR}/aoc-${VERSION}/opt/aws/aws-opentelemetry-collector/doc
-mkdir -p ${WORK_DIR}/aoc-${VERSION}/etc/init
-mkdir -p ${WORK_DIR}/aoc-${VERSION}/etc/systemd/system/
+
+echo "Creating file structure"
+mkdir -p ${AOC_ROOT}/opt/aws/aws-opentelemetry-collector/logs
+mkdir -p ${AOC_ROOT}/opt/aws/aws-opentelemetry-collector/bin
+mkdir -p ${AOC_ROOT}/opt/aws/aws-opentelemetry-collector/etc
+mkdir -p ${AOC_ROOT}/opt/aws/aws-opentelemetry-collector/var
+mkdir -p ${AOC_ROOT}/opt/aws/aws-opentelemetry-collector/doc
+mkdir -p ${AOC_ROOT}/etc/init
+mkdir -p ${AOC_ROOT}/etc/systemd/system/
 
 echo "Copying application files"
-cp LICENSE ${WORK_DIR}/aoc-${VERSION}/opt/aws/aws-opentelemetry-collector/
-cp VERSION ${WORK_DIR}/aoc-${VERSION}/opt/aws/aws-opentelemetry-collector/bin/
-cp build/linux/aoc_linux_${ARCH} ${WORK_DIR}/aoc-${VERSION}/opt/aws/aws-opentelemetry-collector/bin/aoc
-chmod ug+rx ${WORK_DIR}/aoc-${VERSION}/opt/aws/aws-opentelemetry-collector/bin/aoc
+cp LICENSE ${AOC_ROOT}/opt/aws/aws-opentelemetry-collector/
+cp VERSION ${AOC_ROOT}/opt/aws/aws-opentelemetry-collector/bin/
+cp build/linux/aoc_linux_${ARCH} ${AOC_ROOT}/opt/aws/aws-opentelemetry-collector/bin/aoc
+chmod ug+rx ${AOC_ROOT}/opt/aws/aws-opentelemetry-collector/bin/aoc
 
 echo "build source tarball"
-tar -czvf aoc-${VERSION}.tar.gz -C ${WORK_DIR} .
-mv aoc-${VERSION}.tar.gz ${BUILD_ROOT}/SOURCES/
+tar -czvf ${RPM_NAME}-${VERSION}.tar.gz -C ${WORK_DIR} .
+mv ${RPM_NAME}-${VERSION}.tar.gz ${BUILD_ROOT}/SOURCES/
 rm -rf ${WORK_DIR}
 
 echo "Creating the rpm package"
-rpmbuild --define "VERSION $VERSION" --define "_topdir ${BUILD_ROOT}" -bb -v --clean ${SPEC_FILE} --target ${ARCH}
+rpmbuild --define "VERSION $VERSION" --define "RPM_NAME $RPM_NAME" --define "_topdir ${BUILD_ROOT}" -bb -v --clean ${SPEC_FILE} --target ${ARCH}
 
 echo "Copy rpm file to ${DEST}"
 mkdir -p ${DEST}
-cp ${BUILD_ROOT}/RPMS/${ARCH}/aoc-${VERSION}-1.${ARCH}.rpm ${DEST}/aoc.rpm
+cp ${BUILD_ROOT}/RPMS/${ARCH}/${RPM_NAME}-${VERSION}-1.${ARCH}.rpm ${DEST}/${RPM_NAME}.rpm
