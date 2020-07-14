83cad16627619f673456ff73e64cff50369a74a6
Ubuntu <ubuntu@ip-172-31-16-216.us-west-2.compute.internal>
2020-07-14 03:27:56 +0000

    init

--- tools/release/create-release-note.sh
+++ tools/release/create-release-note.sh
@@ -2,9 +2,9 @@
 
 WORKDIR=tools/release
 
-export CHANGE_LOG=$1
-export S3_LINKS=$2
-export IMAGE_LINKS=$3
+export CHANGE_LOG="$1"
+export S3_LINKS="$2"
+export IMAGE_LINKS="$3"
 
 echo ${CHANGE_LOG}
 echo ${S3_LINKS}
@@ -12,8 +12,8 @@ echo ${IMAGE_LINKS}
 
 cp $WORKDIR/release-note.template release-note
 
-perl -i -pe 's/{{changes}}/$ENV{"CHANGE_LOG"}/g' release-note
+perl -i -pe 's/__changes__/$ENV{"CHANGE_LOG"}/g' release-note
 
-perl -i -pe 's/{{s3-links}}/$ENV{"S3_LINKS"}/g' release-note
+perl -i -pe 's/__s3-links__/$ENV{"S3_LINKS"}/g' release-note
 
-perl -i -pe 's/{{image-link}}/$ENV{"IMAGE_LINKS"}/g' release-note
+perl -i -pe 's/__image-link__/$ENV{"IMAGE_LINKS"}/g' release-note
