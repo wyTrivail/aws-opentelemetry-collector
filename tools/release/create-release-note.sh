9b0bfda7e12d16561ef755e2613f4b89692565e3
Ying Wang <wangmyin@amazon.com>
2020-07-13 20:43:06 -0700

    init

--- tools/release/create-release-note.sh
+++ tools/release/create-release-note.sh
@@ -2,18 +2,14 @@
 
 WORKDIR=tools/release
 
-export CHANGE_LOG="$1"
-export S3_LINKS="$2"
-export IMAGE_LINKS="$3"
+export S3_LINKS="$1"
+export IMAGE_LINKS="$2"
 
-echo ${CHANGE_LOG}
 echo ${S3_LINKS}
 echo ${IMAGE_LINKS}
 
-cp $WORKDIR/release-note.template release-note
+cp $WORKDIR/release-note-template.yml .github/release-drafter.yml
 
-perl -i -pe 's/__changes__/$ENV{"CHANGE_LOG"}/g' release-note
+perl -i -pe 's/__s3-links__/$ENV{"S3_LINKS"}/g' .github/release-drafter.yml
 
-perl -i -pe 's/__s3-links__/$ENV{"S3_LINKS"}/g' release-note
-
-perl -i -pe 's/__image-link__/$ENV{"IMAGE_LINKS"}/g' release-note
+perl -i -pe 's/__image-link__/$ENV{"IMAGE_LINKS"}/g' .github/release-drafter.yml
