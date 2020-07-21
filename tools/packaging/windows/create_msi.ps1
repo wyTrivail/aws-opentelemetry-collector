dfa19237404e77e6484463b4bfe2e2200f9e76a7
Shreyas Darwhatkar <darwhs@amazon.com>
2020-07-20 18:14:03 -0700

    MSIBuild through create_msi script

--- tools/packaging/windows/create_msi.ps1
+++ tools/packaging/windows/create_msi.ps1
@@ -1,15 +1,11 @@
-# get the version
-$Version = Get-Content -Path ./VERSION -TotalCount 1 | Out-String
-$Version = $Version.TrimEnd("`n")
-
 # Install the required software
-if(-Not (test-path "C:\Program Files (x86)\WiX Toolset v3.11\bin")){
-    choco install wixtoolset -y
-    echo "::add-path::$WIX\\bin"
-    echo "::add-path::C:\\Program Files (x86)\\WiX Toolset v3.11\\bin"
-}
+
+choco install wixtoolset --force -y
+$env:Path += ";C:\Program Files (x86)\WiX Toolset v3.11\bin"
+refreshenv
 
 # create msi
 candle -arch x64  .\tools\packaging\windows\aws-opentelemetry-collector.wxs
 light .\aws-opentelemetry-collector.wixobj
-mv -f
+
+mv aws-opentelemetry-collector.msi .\build\packages\windows\amd64\
