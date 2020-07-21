968ce21c702b2b3a131847f38185b8d8da3c922a
Ying Wang <wangmyin@amazon.com>
2020-07-20 17:39:27 -0700

    init

--- tools/packaging/windows/create_msi.ps1
+++ tools/packaging/windows/create_msi.ps1
@@ -3,11 +3,9 @@ $Version = Get-Content -Path ./VERSION -TotalCount 1 | Out-String
 $Version = $Version.TrimEnd("`n")
 
 # Install the required software
-if(-Not (test-path "C:\Program Files (x86)\WiX Toolset v3.11\bin")){
-    choco install wixtoolset -y
-    echo "::add-path::$WIX\\bin"
-    echo "::add-path::C:\\Program Files (x86)\\WiX Toolset v3.11\\bin"
-}
+choco install wixtoolset -y
+echo "::add-path::$WIX\\bin"
+echo "::add-path::C:\\Program Files (x86)\\WiX Toolset v3.11\\bin"
 
 # create msi
 candle -arch x64  .\tools\packaging\windows\aws-opentelemetry-collector.wxs
