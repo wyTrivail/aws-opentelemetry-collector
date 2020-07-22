4fafd33830f118bde277695cbd1171737075e707
xiami <xiami@amazon.com>
2020-07-22 15:38:28 -0700

    add lint for build script

--- pkg/defaultcomponents/defaults.go
+++ pkg/defaultcomponents/defaults.go
@@ -6,6 +6,10 @@ import (
 	"go.opentelemetry.io/collector/component"
 	"go.opentelemetry.io/collector/component/componenterror"
 	"go.opentelemetry.io/collector/config"
+	"go.opentelemetry.io/collector/exporter/fileexporter"
+	"go.opentelemetry.io/collector/exporter/loggingexporter"
+	"go.opentelemetry.io/collector/exporter/otlpexporter"
+	"go.opentelemetry.io/collector/exporter/prometheusexporter"
 	"go.opentelemetry.io/collector/service/defaultcomponents"
 )
 
@@ -17,13 +21,20 @@ func Components() (config.Factories, error) {
 		return config.Factories{}, err
 	}
 
+	// Reset the default exporters
+	for k := range factories.Exporters {
+		delete(factories.Exporters, k)
+	}
+
 	exporters := []component.ExporterFactoryBase{
 		&awsxrayexporter.Factory{},
 		&awsemfexporter.Factory{},
+		&prometheusexporter.Factory{},
+		&loggingexporter.Factory{},
+		&fileexporter.Factory{},
+		&otlpexporter.Factory{},
 	}
-	for _, exp := range factories.Exporters {
-		exporters = append(exporters, exp)
-	}
+
 	factories.Exporters, err = component.MakeExporterFactoryMap(exporters...)
 	if err != nil {
 		errs = append(errs, err)
