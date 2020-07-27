223352bddde69dcfe67453f3c40a0832b4174a01
Ying Wang <wangmyin@amazon.com>
2020-07-26 20:36:33 -0700

    rpmbuild: change the permission of logs to aoc user

--- tools/packaging/linux/build.spec
+++ tools/packaging/linux/build.spec
@@ -23,22 +23,23 @@ cp -fa * ${RPM_BUILD_ROOT}
 %dir /opt/aws/aws-opentelemetry-collector/bin
 %dir /opt/aws/aws-opentelemetry-collector/doc
 %dir /opt/aws/aws-opentelemetry-collector/etc
-%dir /opt/aws/aws-opentelemetry-collector/logs
-%dir /opt/aws/aws-opentelemetry-collector/var
+%dir %attr(-, aoc, aoc) /opt/aws/aws-opentelemetry-collector/logs
+%dir %attr(-, aoc, aoc) /opt/aws/aws-opentelemetry-collector/var
+
 /opt/aws/aws-opentelemetry-collector/bin/aws-opentelemetry-collector
 /opt/aws/aws-opentelemetry-collector/bin/aws-opentelemetry-collector-ctl
 /opt/aws/aws-opentelemetry-collector/bin/VERSION
 /opt/aws/aws-opentelemetry-collector/LICENSE
+/opt/aws/aws-opentelemetry-collector/RELEASE_NOTE
 /opt/aws/aws-opentelemetry-collector/etc/config.yaml
 /opt/aws/aws-opentelemetry-collector/etc/.env
+
 /etc/init/aws-opentelemetry-collector.conf
 /etc/systemd/system/aws-opentelemetry-collector.service
 /usr/bin/aws-opentelemetry-collector-ctl
-#
-#/usr/bin/aws-opentelemetry-collector
-#/etc/amazon/aws-opentelemetry-collector
-#/var/log/amazon/aws-opentelemetry-collector
-#/var/run/amazon/aws-opentelemetry-collector
+/etc/amazon/aws-opentelemetry-collector
+/var/log/amazon/aws-opentelemetry-collector
+/var/run/amazon/aws-opentelemetry-collector
 
 %pre
 # Stop the agent before upgrades.
@@ -48,15 +49,18 @@ if [ $1 -ge 2 ]; then
     fi
 fi
 
+# create group
 if ! grep "^aoc:" /etc/group >/dev/null 2>&1; then
     groupadd -r aoc >/dev/null 2>&1
     echo "create group aoc, result: $?"
 fi
 
+# create user
 if ! id aoc >/dev/null 2>&1; then
-    useradd -r -M aoc -d /home/aoc -g aoc >/dev/null 2>&1
+    useradd -r -M aoc -d /home/aoc -g aoc -c "AWS Observability Collector" -s $(test -x /sbin/nologin && echo /sbin/nologin || (test -x /usr/sbin/nologin && echo /usr/sbin/nologin || (test -x /bin/false && echo /bin/false || echo /bin/sh))) >/dev/null 2>&1
     echo "create user aoc, result: $?"
 fi
+
 
 %clean
 rm -rf ${RPM_BUILD_ROOT}
--- tools/packaging/linux/create_rpm.sh
+++ tools/packaging/linux/create_rpm.sh
@@ -24,22 +24,39 @@ mkdir -p ${AOC_ROOT}/opt/aws/aws-opentelemetry-collector/doc
 mkdir -p ${AOC_ROOT}/etc/init
 mkdir -p ${AOC_ROOT}/etc/systemd/system
 mkdir -p ${AOC_ROOT}/usr/bin
+mkdir -p ${AOC_ROOT}/etc/amazon
+mkdir -p ${AOC_ROOT}/var/log/amazon
+mkdir -p ${AOC_ROOT}/var/run/amazon
 
 echo "Copying application files"
+# License, version, release note... 
 cp LICENSE ${AOC_ROOT}/opt/aws/aws-opentelemetry-collector/
 cp VERSION ${AOC_ROOT}/opt/aws/aws-opentelemetry-collector/bin/
+cp docs/releases/${VERSION}.md ${AOC_ROOT}/opt/aws/aws-opentelemetry-collector/RELEASE_NOTE
 
+# binary
 cp build/linux/aoc_linux_${ARCH} ${AOC_ROOT}/opt/aws/aws-opentelemetry-collector/bin/aws-opentelemetry-collector
+# ctl
 cp tools/ctl/linux/aws-opentelemetry-collector-ctl ${AOC_ROOT}/opt/aws/aws-opentelemetry-collector/bin/
+# default config
 cp config.yaml ${AOC_ROOT}/opt/aws/aws-opentelemetry-collector/etc
+# .env
 cp .env ${AOC_ROOT}/opt/aws/aws-opentelemetry-collector/etc
+# service config
 cp tools/packaging/linux/aws-opentelemetry-collector.service ${AOC_ROOT}/etc/systemd/system/
 cp tools/packaging/linux/aws-opentelemetry-collector.conf ${AOC_ROOT}/etc/init/
+
+echo "assign permission to the files"
 chmod ug+rx ${AOC_ROOT}/opt/aws/aws-opentelemetry-collector/bin/aws-opentelemetry-collector
 chmod ug+rx ${AOC_ROOT}/opt/aws/aws-opentelemetry-collector/bin/aws-opentelemetry-collector-ctl
 chmod ug+rx ${AOC_ROOT}/opt/aws/aws-opentelemetry-collector/etc/config.yaml
 chmod ug+rx ${AOC_ROOT}/opt/aws/aws-opentelemetry-collector/etc/.env
+
+echo "create symlinks"
 ln -f -s /opt/aws/aws-opentelemetry-collector/bin/aws-opentelemetry-collector-ctl ${AOC_ROOT}/usr/bin/aws-opentelemetry-collector-ctl
+ln -f -s /opt/aws/aws-opentelemetry-collector/etc ${AOC_ROOT}/etc/amazon/aws-opentelemetry-collector
+ln -f -s /opt/aws/aws-opentelemetry-collector/logs ${AOC_ROOT}/var/log/amazon/aws-opentelemetry-collector
+ln -f -s /opt/aws/aws-opentelemetry-collector/var ${AOC_ROOT}/var/run/amazon/aws-opentelemetry-collector
 
 echo "build source tarball"
 tar -czvf ${RPM_NAME}-${VERSION}.tar.gz -C ${WORK_DIR} .
@@ -47,7 +64,7 @@ mv ${RPM_NAME}-${VERSION}.tar.gz ${BUILD_ROOT}/SOURCES/
 rm -rf ${WORK_DIR}
 
 echo "Creating the rpm package"
-rpmbuild --define "VERSION $VERSION" --define "RPM_NAME $RPM_NAME" --define "_topdir ${BUILD_ROOT}" -bb -v --clean ${SPEC_FILE} --target ${ARCH}
+rpmbuild --define "VERSION $VERSION" --define "RPM_NAME $RPM_NAME" --define "_topdir ${BUILD_ROOT}" -bb -v --clean ${SPEC_FILE} --target ${ARCH}-linux
 
 echo "Copy rpm file to ${DEST}"
 mkdir -p ${DEST}
